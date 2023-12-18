9e1e72dc7ca4f194e6da68585261c53cf349ea86
Kappis06 <kasper.noren123@gmail.com>
2023-12-18 15:20:01 +0100

    fgh

--- TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/MouseAim.cs
+++ TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/MouseAim.cs
@@ -23,12 +23,12 @@ public class MouseAim : MonoBehaviour
 
 
 
-        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        //Vector3 joystickMulti = new Vector3(14, 7, 0);
-        //Vector3 joystickInput = new Vector3(Input.GetAxis(""), Input.GetAxis(""));
-        //Vector3 joystickResult = new Vector3(joystickMulti.x * joystickInput.x, joystickMulti.y * joystickInput.y, joystickMulti.z * joystickInput.z);
+        //Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 joystickMulti = new Vector3(14, 7, 0);
+        Vector3 joystickInput = new Vector3(Input.GetAxis("Horizontal_R"), Input.GetAxis("Vertical_R"), 0);
+        Vector3 joystickResult = new Vector3(joystickMulti.x * joystickInput.x, joystickMulti.y * joystickInput.y, joystickMulti.z * joystickInput.z);
 
-        Vector3 targetPos = (player.position + /*joystickResult*/ mousePos) / 2f;
+        Vector3 targetPos = (player.position + joystickResult /*mousePos*/) / 2f;
 
         targetPos.x = Mathf.Clamp(targetPos.x, - threshold + player.position.x, threshold + player.position.x);
         targetPos.y = Mathf.Clamp(targetPos.y, - threshold + player.position.y, threshold + player.position.y);
