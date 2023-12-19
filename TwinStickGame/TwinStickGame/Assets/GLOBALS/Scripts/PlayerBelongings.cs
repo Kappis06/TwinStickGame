72cfe5a8076f54d685bc7d8fbe7d593006cda297
Kappis06 <kasper.noren123@gmail.com>
2023-12-19 10:58:33 +0100

    sertgd

--- TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/PlayerBelongings.cs
+++ TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/PlayerBelongings.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerBelongings : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Beloning;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
