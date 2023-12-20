072976f410610f2b0443d87a9159a5bc46e4c8e4
Kappis06 <kasper.noren123@gmail.com>
2023-12-20 12:49:54 +0100

    asfgsehshsdhsfghsrhdfghdfgdfghdftydfrthgdfhgdfthdfth

--- TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/MainMenu.cs
+++ TwinStickGame/TwinStickGame/Assets/GLOBALS/Scripts/MainMenu.cs
@@ -2,16 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject helpScreen;
+    public GameObject creditsScreen;
+
+    public EventSystem eventSystem;
+    public GameObject playButton;
+    public GameObject helpReturnButton;
+    public GameObject creditsReturnButton;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        helpScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Help()
+    {
+        helpScreen.SetActive(true);
+        mainMenu.SetActive(false);
+        eventSystem.SetSelectedGameObject(helpReturnButton);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        creditsScreen.SetActive(true);
+        mainMenu.SetActive(false);
+        eventSystem.SetSelectedGameObject(creditsReturnButton);
+    }
+
+    public void GoBack()
+    {
+        mainMenu.SetActive(true);
+        helpScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        eventSystem.SetSelectedGameObject(playButton);
     }
 }
